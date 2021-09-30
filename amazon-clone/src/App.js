3f7fcf45122189034a8fbf9d6fcb1f5a29873009
MBusto <mikebusto4@gmail.com>
2021-09-30 13:06:16 -0400

    feat: Starting the Home comp body

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import './Header';
 import Header from './Header'
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       {/* Header/NavBar */}
       {/* Home/body */}
     </div>
---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className=" home">
+            <p> HOME COMP</p>
+        </div>
+    )
+}
+
+export default Home;
