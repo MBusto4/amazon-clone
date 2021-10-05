ffa7509c98d0b90dc1b00857746a1960380d45f8
MBusto <mikebusto4@gmail.com>
2021-10-05 15:34:37 -0400

    feat: setting up login auth

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
+
+
 function Login() {
+
+
+    //React Hooks to keep track of state for the email and password for a user
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const signIn = e => {
+        e.preventDefault()
+        //some firebase login logic
+    }
+    const register = e => {
+        e.preventDefault()
+        //some firebase register logic
+    }
+
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -14,13 +32,26 @@ function Login() {
                 <h1>Sign In</h1>
                 <form action="">
                     <h5>E-mail</h5>
-                    <input type='text'></input>
+                    <input
+                        type='text'
+                        value={email}
+                        //this is how we keep track of what a user is typing
+                        onChange={e => setEmail(e.target.value)}>
+
+                    </input>
 
                     <h5>Password</h5>
-                    <input type='password'></input>
+                    <input
+                        type='password'
+                        value={password}
+                        //this is how we keep track of what a user is typing
+                        onChange={e => setPassword(e.target.value)}
+                    ></input>
 
                     <button
                         className='login__signInButton'
+                        onClick={signIn}
+                        type='submit'
                     >Sign In</button>
                 </form>
 
@@ -28,6 +59,7 @@ function Login() {
                     see our Privacy Notice, our Cookies Notice and our Internet-Based Ads Notice.
                 </p>
                 <button
+                    onClick={register}
                     className='login__registerButton'
                 >Create an Amazon-Clone Account</button>
             </div>
